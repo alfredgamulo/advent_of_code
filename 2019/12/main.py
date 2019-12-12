8c9e6283d1620f6144765dd2bf33dc03d373b7a6
Alfred Gamulo <alfred.gamulo@gmail.com>
2019-12-12 18:02:37 +0000

    day 12 part 1

---
+++ 2019/12/main.py
@@ -0,0 +1,73 @@
+class Moon():
+    def __init__(self, x, y, z):
+        self.x = x
+        self.y = y
+        self.z = z
+        self.velocity = (0, 0, 0)
+
+    def positions(self):
+        return self.x, self.y, self.z
+    
+    def update_positions(self):
+        self.x += self.velocity[0]
+        self.y += self.velocity[1]
+        self.z += self.velocity[2]
+
+    def update_velocity(self, x, y, z):
+        ox, oy, oz = self.velocity
+        self.velocity = (ox+x, oy+y, oz+z)
+        
+def read_input(file):
+    moons = []
+    with open(file) as f:
+        for r in f.readlines():
+            x, y, z = map(str.strip, r.strip().split(','))
+            x = int(x[3:])
+            y = int(y[2:])
+            z = int(z[2:-1])
+            moons.append(Moon(x, y, z))
+    return moons
+
+
+def part1():
+    moons = read_input("input")
+    for _ in range(1000):
+        for m in moons:
+            other_moons = moons[:]
+            other_moons.pop(moons.index(m))
+            for o in other_moons:
+                m_x, m_y, m_z = m.positions()
+                o_x, o_y, o_z = o.positions()
+                
+                if m_x > o_x:
+                    v_x = -1
+                elif m_x < o_x:
+                    v_x = 1
+                else:
+                    v_x = 0
+                    
+                if m_y > o_y:
+                    v_y = -1
+                elif m_y < o_y:
+                    v_y = 1
+                else:
+                    v_y = 0
+                    
+                if m_z > o_z:
+                    v_z = -1
+                elif m_z < o_z:
+                    v_z = 1
+                else:
+                    v_z = 0
+                m.update_velocity(v_x, v_y, v_z)
+        for m in moons:
+            m.update_positions()
+    
+    calc = 0
+    for m in moons:
+        pot = sum(map(abs, m.positions()))
+        kin = sum(map(abs, m.velocity))
+        calc += pot*kin
+
+    print(calc)
+part1()
