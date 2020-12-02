af3428c9591965c1bbeb8fbedbbf656208c9c768
Alfred Gamulo <alfred.gamulo@gmail.com>
2020-12-02 14:00:07 +0000

    day 2 2020

---
+++ 2020/02/main.py
@@ -0,0 +1,22 @@
+from collections import Counter
+
+
+part1 = 0
+part2 = 0
+
+with open("input") as f:
+    for line in f.readlines():
+        fields = line.split(' ')
+        limits = tuple(map(int, fields[0].split('-')))
+        letter = fields[1].strip(':')
+        passwd = fields[2]
+        
+        c = Counter(passwd)
+        if limits[0] <= c[letter] <= limits[1]:
+            part1 += 1
+
+        if bool(passwd[limits[0]-1] == letter) ^ bool(passwd[limits[1]-1] == letter):
+            part2 += 1
+
+print("Part 1:", part1)
+print("Part 2:", part2)    
