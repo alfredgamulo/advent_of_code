ce94220f21d740d7da8611a7c43e4f9483e47edc
Alfred Gamulo <alfred.gamulo@gmail.com>
2020-12-05 13:10:22 +0000

    day 05 2020

--- 2020/05/main.py
+++ 2020/05/main.py
@@ -3,23 +3,9 @@ f = open("input")
 seat_ids = []
 
 for line in f.read().splitlines():
-    rows = line[:7]
-    cols = line[7:]
-    
-    row_range = list(range(128))
-    col_range = list(range(8))
-
-    for r in rows:
-        index = len(row_range) // 2
-        row_range = row_range[:index] if r == "F" else row_range[index:]
-    
-    for c in cols:
-        index = len(col_range) // 2
-        col_range = col_range[:index] if c == "L" else col_range[index:]
-    
-    val = row_range[0]*8+col_range[0]
+    bs = "".join(["0" if l in ("F", "L") else "1" for l in line])
+    val = int(bs, 2)
     seat_ids.append(val)
-
 
 seat_ids.sort()
 
