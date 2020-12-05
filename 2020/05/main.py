9a16fc1504ce5845acbc166988b0f8eb6e889b2c
Alfred Gamulo <alfred.gamulo@gmail.com>
2020-12-05 15:54:11 +0000

    day 05 2020 polish

--- 2020/05/main.py
+++ 2020/05/main.py
@@ -1,20 +1,7 @@
-f = open("input")
+with open("input") as f:
+    seat_ids = set(int("".join(["0" if l in ("F", "L") else "1" for l in line]), 2) for line in f.read().splitlines())
 
-seat_ids = []
-
-for line in f.read().splitlines():
-    bs = "".join(["0" if l in ("F", "L") else "1" for l in line])
-    val = int(bs, 2)
-    seat_ids.append(val)
-
-seat_ids.sort()
-
-# part 1
-print(seat_ids[-1])
-
-# part 2
-offset = seat_ids[0]
-for i, s in enumerate(seat_ids):
-    if i != s-offset:
-        print(s-1)
-        break
+high = max(seat_ids)
+print(high)
+miss = next(s for s in range(high, 0, -1) if s not in seat_ids)
+print(miss)
