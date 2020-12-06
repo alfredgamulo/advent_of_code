8bc2b4d93d0d219f1fac5801031fdec9dbed181c
Alfred Gamulo <alfred.gamulo@gmail.com>
2020-12-06 13:24:58 +0000

    day 06 2020

---
+++ 2020/06/main.py
@@ -0,0 +1,11 @@
+from collections import Counter
+part1 = 0
+part2 = 0
+with open("input") as f:
+    for group in f.read().split("\n\n"):
+        members = group.split()
+        s = Counter("".join(members))
+        part1 += len(s)
+        part2 += sum(1 for f in s if s[f] >= len(members))
+print(part1)
+print(part2)
