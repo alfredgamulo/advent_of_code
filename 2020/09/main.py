d99a66854212d134628a8449438fcc2f83f232e6
Alfred Gamulo <alfred.gamulo@gmail.com>
2020-12-09 13:54:56 +0000

    day 09 2020

--- 2020/09/main.py
+++ 2020/09/main.py
@@ -4,14 +4,27 @@ with open("input") as f:
 pos = 25
 
 # Part 1
-part1 = -1
+part1 = None
 for c in cypher[pos:]:
     pre = set(cypher[pos-25:pos])
-    if any([c-p in pre for p in pre]):
-        pos += 1
-        continue
-    else:
-        part1 = c
+    if not any(c-p in pre for p in pre):
+        part1 = (pos, c)
         break
+    pos += 1
 
-print("Part 1:", part1)
+print("Part 1:", part1[1])
+
+# Part 2
+hi = part1[0]-1
+lo = part1[0]-2
+t = part1[1]
+while lo > 0 and hi > 0:
+    r = cypher[lo:hi]
+    s = sum(r)
+    if s == t:
+        print("Part 2:", min(r) + max(r))
+        break
+    if s < t:
+        lo -= 1
+    else:
+        hi -= 1
