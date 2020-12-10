a85b2113a201537aafe813d20c731568609ca9f0
Alfred Gamulo <agamulo@amazon.com>
2020-12-10 15:54:01 -0500

    day 10 2020

--- 2020/10/main.py
+++ 2020/10/main.py
@@ -1,25 +1,22 @@
-#! /usr/local/bin/python3
-
 from collections import Counter
 from itertools import combinations
 import time
 start_time = time.time()
 
-joltages = [0]
 with open("input") as f:
-    joltages.extend(sorted(map(int, f.readlines())))
+    joltages = list(sorted(map(int, f.readlines()), reverse=True)) + [0]
 
-c = Counter(y-x for x, y in zip(joltages, joltages[1:]))
+c = Counter(x-y for x, y in zip(joltages, joltages[1:]))
 print("Part 1:", c[1]*(c[3]+1))
 
 d = {}
 for x, y in combinations(joltages, 2):
-    if y - x <= 3:
-        d.setdefault(x, []).append(y)
+    if x - y <= 3:
+        d.setdefault(y, []).append(x)
 
 branch_count = {}
-for k in reversed(d): 
-    branch_count[k] = sum(branch_count.get(v, 1) for v in d.get(k))
+for k, values in d.items(): 
+    branch_count[k] = sum(branch_count.get(v, 1) for v in values)
 
 print("Part 2:", branch_count.get(0))
 print("--- %s seconds ---" % (time.time() - start_time))
