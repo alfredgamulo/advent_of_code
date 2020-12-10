5ec7963d965c74bd55d8b3433cb5742a8b66f503
Alfred Gamulo <agamulo@amazon.com>
2020-12-10 16:02:19 -0500

    day 10 2020

--- 2020/10/main.py
+++ 2020/10/main.py
@@ -1,4 +1,4 @@
-from collections import Counter
+from collections import Counter, defaultdict
 from itertools import combinations
 import time
 start_time = time.time()
@@ -9,14 +9,14 @@ with open("input") as f:
 c = Counter(x-y for x, y in zip(joltages, joltages[1:]))
 print("Part 1:", c[1]*(c[3]+1))
 
-d = {}
+d = defaultdict(list)
 for x, y in combinations(joltages, 2):
     if x - y <= 3:
-        d.setdefault(y, []).append(x)
+        d[y].append(x)
 
-branch_count = {}
+branch_count = defaultdict(lambda: 1)
 for k, values in d.items(): 
-    branch_count[k] = sum(branch_count.get(v, 1) for v in values)
+    branch_count[k] = sum(branch_count[v] for v in values)
 
 print("Part 2:", branch_count.get(0))
 print("--- %s seconds ---" % (time.time() - start_time))
