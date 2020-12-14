0b9eda4913b682b74d8866b2eedb99e3bf8e0d61
Alfred Gamulo <alfred.gamulo@gmail.com>
2020-12-14 15:33:37 +0000

    day 14 2020

---
+++ 2020/14/main.py
@@ -0,0 +1,53 @@
+import time as timer
+start_time = timer.time()
+
+from collections import defaultdict
+import re
+
+# Part 1:
+memory1 = defaultdict(int)
+mask_1 = None
+mask_0 = None
+# Part 2:
+memory2 = defaultdict(int)
+mask_10 = None
+mask_xx = None
+
+with open("input") as f:
+    for line in f.readlines():
+        if line.startswith("mask"):
+            mask = line.split('=')[1].strip()
+
+            # Part 1:
+            mask_1 = "".join(map(str,[1 if s == "1" else 0 for s in mask]))
+            mask_0 = "".join(map(str,[1 if s == "0" else 0 for s in mask]))
+            mask_1 = int(mask_1,2)
+            mask_0 = int(mask_0,2)
+
+            # Part 2:
+            mask_10 = "".join(map(str,[s if s != "X" else 0 for s in mask]))
+            mask_xx = "".join(map(str,[1 if s == "X" else 0 for s in mask]))
+            mask_10 = int(mask_10, 2)
+            mask_xx = int(mask_xx, 2)
+        else:
+            nums = list(map(int,re.findall(r'\d+', line)))
+            mem = nums[0]
+            val = nums[1]
+
+            # Part 1:
+            result = val | mask_1 # apply mask 1
+            result = (result | mask_0) - mask_0 # apply mask 0
+            memory1[mem] = result
+
+            # Part 2:
+            mem_0 = mem | mask_10
+            mems = [mem_0]
+            for i in range(36):
+                if mask_xx & 2**i > 0:
+                    mems.extend(list(m ^ 2**i for m in mems))
+            for m in mems:
+                memory2[m] = val
+
+print("Part 1:", sum(memory1.values()))
+print("Part 2:", sum(memory2.values()))
+print("--- %s millis ---" % ((timer.time() - start_time)*1000))
