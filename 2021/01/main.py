90c2caa7bbe993f0e626cd17a5496ebae0be2c3a
Alfred Gamulo <alfred.gamulo@gmail.com>
2021-12-01 11:25:53 -0500

    refactor

--- 2021/01/main.py
+++ 2021/01/main.py
@@ -7,7 +7,7 @@ def count_increasing(depths):
     return sum([1 for x, y in zip(depths, depths[1:]) if y > x])
 
 
-print("Part 1: ", count_increasing(lines))
+print("Part 1:", count_increasing(lines))
 
 windows = list(map(sum, zip(lines, lines[1:], lines[2:])))
-print("Part 2: ", count_increasing(windows))
+print("Part 2:", count_increasing(windows))
