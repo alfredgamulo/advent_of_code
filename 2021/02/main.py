52da1583693a348365600f1583c7df04580a90ab
Alfred Gamulo <alfred.gamulo@gmail.com>
2021-12-02 00:46:12 -0500

    refactor

--- 2021/02/main.py
+++ 2021/02/main.py
@@ -4,32 +4,37 @@ lines = sys.stdin.readlines()
 
 position = (0, 0)
 
+
+def move(position, step):
+    return tuple(x + y for x, y in zip(position, step))
+
+
 for line in lines:
-    instruction, distance = line.split()
-    distance = int(distance)
-    if instruction == "forward":
-        position = tuple(x + y for x, y in zip(position, (distance, 0)))
-    if instruction == "down":
-        position = tuple(x + y for x, y in zip(position, (0, distance)))
+    instruction, number = line.split()
+    number = int(number)
     if instruction == "up":
-        position = tuple(x + y for x, y in zip(position, (0, -distance)))
+        position = move(position, (0, -number))
+    if instruction == "down":
+        position = move(position, (0, number))
+    if instruction == "forward":
+        position = move(position, (number, 0))
 
 print("Part 1:", position[0] * position[1])
 
 position = (0, 0, 0)
 
 for line in lines:
-    instruction, distance = line.split()
-    distance = int(distance)
+    instruction, number = line.split()
+    number = int(number)
+    if instruction == "up":
+        position = move(position, (0, 0, -number))
+    if instruction == "down":
+        position = move(position, (0, 0, number))
     if instruction == "forward":
         position = (
-            position[0] + distance,
-            position[1] + (distance * position[2]),
+            position[0] + number,
+            position[1] + number * position[2],
             position[2],
         )
-    if instruction == "down":
-        position = tuple(x + y for x, y in zip(position, (0, 0, distance)))
-    if instruction == "up":
-        position = tuple(x + y for x, y in zip(position, (0, 0, -distance)))
 
 print("Part 2:", position[0] * position[1])
