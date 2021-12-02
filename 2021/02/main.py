887e8065e106a116dbeb4bb9ce415c8469fa9bc6
Alfred Gamulo <alfred.gamulo@gmail.com>
2021-12-02 08:42:34 -0500

    Update main.py

--- 2021/02/main.py
+++ 2021/02/main.py
@@ -8,29 +8,29 @@ def move(position, step):
 
 
 position = (0, 0)
+operate = {
+    "up": lambda n: move(position, (0, -n)),
+    "down": lambda n: move(position, (0, n)),
+    "forward": lambda n: move(position, (n, 0)),
+}
 for line in lines:
     instruction, number = line.split()
-    operate = {
-        "up": lambda n: move(position, (0, -n)),
-        "down": lambda n: move(position, (0, n)),
-        "forward": lambda n: move(position, (n, 0)),
-    }
     position = operate[instruction](int(number))
 
 print("Part 1:", position[0] * position[1])
 
 position = (0, 0, 0)
+operate = {
+    "up": lambda n: move(position, (0, 0, -n)),
+    "down": lambda n: move(position, (0, 0, n)),
+    "forward": lambda n: (
+        position[0] + n,
+        position[1] + n * position[2],
+        position[2],
+    ),
+}
 for line in lines:
     instruction, number = line.split()
-    operate = {
-        "up": lambda n: move(position, (0, 0, -n)),
-        "down": lambda n: move(position, (0, 0, n)),
-        "forward": lambda n: (
-            position[0] + n,
-            position[1] + n * position[2],
-            position[2],
-        ),
-    }
     position = operate[instruction](int(number))
 
 print("Part 2:", position[0] * position[1])
