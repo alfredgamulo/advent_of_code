93e882b69bd2d442da6184d16611615c43264d27
Alfred Gamulo <alfred.gamulo@gmail.com>
2021-12-01 23:10:39 -0500

    luacheck

--- 2021/01/main.lua
+++ 2021/01/main.lua
@@ -4,9 +4,8 @@ local print = print
 local table = table
 local tonumber = tonumber
 
-
 local lines = {}
-for line in io.lines() do 
+for line in io.lines() do
     table.insert(lines, tonumber(line))
 end
 
@@ -19,21 +18,21 @@ end
 
 print("Part 1: ", count)
 
-function sum(t)
-    local count = 0
-    for i, x in ipairs(t) do
-        count = count + x
+local function sum(t)
+    local s = 0
+    for _, x in ipairs(t) do
+        s = s + x
     end
-    return count
+    return s
 end
 
-local count = 0
-for i, l in ipairs(lines) do
+local window_count = 0
+for i, _ in ipairs(lines) do
     if lines[i-3] then
         if sum({table.unpack(lines, i-3, i-1)}) < sum({table.unpack(lines, i-2, i)}) then
-            count = count + 1
+            window_count = window_count + 1
         end
     end
 end
 
-print("Part 2: ", count)
+print("Part 2: ", window_count)
