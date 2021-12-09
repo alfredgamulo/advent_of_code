db47d752b4eb600b3e61fa1c778825d7cb36a84d
Alfred Gamulo <alfred.gamulo@gmail.com>
2021-12-09 12:18:04 -0500

    dedent

--- 2021/09/main.py
+++ 2021/09/main.py
@@ -1,5 +1,6 @@
 import sys
 from collections import deque
+from itertools import product
 
 matrix = []
 for line in map(str.strip, sys.stdin.readlines()):
@@ -8,17 +9,16 @@ len_i = len(matrix)
 len_j = len(matrix[0])
 
 lows = {}
-for i in range(len_i):
-    for j in range(len_j):
-        if i > 0 and matrix[i][j] >= matrix[i - 1][j]:
-            continue
-        if j > 0 and matrix[i][j] >= matrix[i][j - 1]:
-            continue
-        if i < len_i - 1 and matrix[i][j] >= matrix[i + 1][j]:
-            continue
-        if j < len_j - 1 and matrix[i][j] >= matrix[i][j + 1]:
-            continue
-        lows[(i, j)] = matrix[i][j]
+for i, j in product(range(len_i), range(len_j)):
+    if i > 0 and matrix[i][j] >= matrix[i - 1][j]:
+        continue
+    if j > 0 and matrix[i][j] >= matrix[i][j - 1]:
+        continue
+    if i < len_i - 1 and matrix[i][j] >= matrix[i + 1][j]:
+        continue
+    if j < len_j - 1 and matrix[i][j] >= matrix[i][j + 1]:
+        continue
+    lows[(i, j)] = matrix[i][j]
 print("Part 1:", sum(lows.values()) + len(lows.values()))
 
 basins = []
@@ -29,20 +29,21 @@ for low in lows.keys():
     size = 0
     while d:
         i, j = d.popleft()
-        if matrix[i][j] < 9:
-            size += 1
-            if i > 0 and (p:=(i - 1, j)) not in v:
-                v.add(p)
-                d.append(p)
-            if j > 0 and (p:=(i, j - 1)) not in v:
-                v.add(p)
-                d.append(p)
-            if i < len_i - 1 and (p:=(i + 1, j)) not in v:
-                v.add(p)
-                d.append(p)
-            if j < len_j - 1 and (p:=(i, j + 1)) not in v:
-                v.add(p)
-                d.append(p)
+        if matrix[i][j] == 9:
+            continue
+        size += 1
+        if i > 0 and (p:=(i - 1, j)) not in v:
+            v.add(p)
+            d.append(p)
+        if j > 0 and (p:=(i, j - 1)) not in v:
+            v.add(p)
+            d.append(p)
+        if i < len_i - 1 and (p:=(i + 1, j)) not in v:
+            v.add(p)
+            d.append(p)
+        if j < len_j - 1 and (p:=(i, j + 1)) not in v:
+            v.add(p)
+            d.append(p)
     basins.append(size)
 basins.sort()
 print("Part 2:", basins[-1] * basins[-2] * basins[-3])
