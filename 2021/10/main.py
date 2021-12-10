8abd97d658fe741fb1f498188cf042b6e1619dcb
Alfred Gamulo <alfred.gamulo@gmail.com>
2021-12-10 00:56:36 -0500

    undent

--- 2021/10/main.py
+++ 2021/10/main.py
@@ -15,23 +15,20 @@ for line in map(str.strip, sys.stdin.readlines()):
     for c in line:
         if c in openers:
             d.append(c)
-            continue
-        if c in endings:
+        elif c in endings:
             x = d.pop()
             if matches[x] != c:
                 part1 += syntax_points[c]
                 break
     else:
-        if d:
-            score = 0
-            while d:
-                x = d.pop()
-                if x in openers:
-                    score = score * 5 + autocomplete_points[matches[x]]
-                    continue
-                if x in endings:
-                    d.pop()
-            part2.append(score)
+        score = 0
+        while d:
+            x = d.pop()
+            if x in openers:
+                score = score * 5 + autocomplete_points[matches[x]]
+            elif x in endings:
+                d.pop()
+        part2.append(score)
 
 print("Part 1:", part1)
 print("Part 2:", sorted(part2)[len(part2) // 2])
