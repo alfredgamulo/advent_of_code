e19903d8c6fa1c5f5bd7ab5ece0fb4c945d3c86e
Alfred Gamulo <alfred.gamulo@gmail.com>
2021-12-10 01:03:54 -0500

    tidy

--- 2021/10/main.py
+++ 2021/10/main.py
@@ -1,9 +1,7 @@
 import sys
 from collections import deque
 
-
 openers = ("(", "[", "{", "<")
-endings = (")", "]", "}", ">")
 matches = {"(": ")", "[": "]", "{": "}", "<": ">"}
 syntax_points = {")": 3, "]": 57, "}": 1197, ">": 25137}
 autocomplete_points = {")": 1, "]": 2, "}": 3, ">": 4}
@@ -15,18 +13,15 @@ for line in map(str.strip, sys.stdin.readlines()):
     for c in line:
         if c in openers:
             d.append(c)
-        elif c in endings:
-            x = d.pop()
-            if matches[x] != c:
-                part1 += syntax_points[c]
-                break
+        elif c != matches[d.pop()]:
+            part1 += syntax_points[c]
+            break
     else:
         score = 0
         while d:
-            x = d.pop()
-            if x in openers:
+            if (x := d.pop()) in openers:
                 score = score * 5 + autocomplete_points[matches[x]]
-            elif x in endings:
+            else:
                 d.pop()
         part2.append(score)
 
