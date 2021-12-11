1bf94265325b1568d71cef33aa04fcf918c40ff8
Alfred Gamulo <alfred.gamulo@gmail.com>
2021-12-11 18:06:05 -0500

    cleanup and lint

--- 2021/11/main.py
+++ 2021/11/main.py
@@ -13,21 +13,22 @@ ns = [(-1, -1), (-1, 0), (-1, 1), (0, -1), (0, 1), (1, -1), (1, 0), (1, 1)]
 def count_flashes(m):
     flashes = 0
     for i, j in product(range(li), range(lj)):
-        if m[i][j] > 9:
-            m[i][j] = 0
-            for n in ns:
-                x = i + n[0]
-                y = j + n[1]
-                if x in range(li) and y in range(lj) and matrix[x][y] > 0:
-                    matrix[x][y] += 1
-            flashes += 1 + count_flashes(m)
+        if m[i][j] <= 9:
+            continue
+        m[i][j] = 0
+        for n in ns:
+            x = i + n[0]
+            y = j + n[1]
+            if x in range(li) and y in range(lj) and m[x][y] > 0:
+                m[x][y] += 1
+        flashes += 1 + count_flashes(m)
     return flashes
 
 
 f100 = 0
 days = 0
 flashes = 0
-while flashes != (li) * (lj):
+while flashes != li * lj:
     for i, j in product(range(li), range(lj)):
         matrix[i][j] += 1
     flashes = count_flashes(matrix)
