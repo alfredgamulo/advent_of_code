005b52f914ad4e0ac0e9fc8887013ff64808b6cd
Alfred Gamulo <alfred.gamulo@gmail.com>
2021-12-13 12:00:08 -0500

    lint

--- 2021/13/main.py
+++ 2021/13/main.py
@@ -1,12 +1,11 @@
 import sys
 from collections import defaultdict
-import pprint
 
-manual = sys.stdin.read().split("\n\n")
-dots = {(int(m.split(',')[0]),int(m.split(',')[1])) for m in manual[0].split()}
-fold = [f.split()[2] for f in manual[1].strip().split('\n')]
+lines = sys.stdin.read().split("\n\n")
+dots = {(int(m.split(",")[0]), int(m.split(",")[1])) for m in lines[0].split()}
+fold = [f.split()[2] for f in lines[1].strip().split("\n")]
 
-for i,f in enumerate(fold):
+for i, f in enumerate(fold):
     pos, coord = f.split("=")
     pos = ["x", "y"].index(pos)
     new_dots = set()
@@ -15,7 +14,7 @@ for i,f in enumerate(fold):
         new_dot[pos] = int(coord) - abs(int(coord) - dot[pos])
         new_dots.add(tuple(new_dot))
     dots = new_dots
-    
+
     if i == 0:
         print("Part 1:", len(dots))
 
@@ -23,7 +22,7 @@ print("Part 2:")
 
 paper = defaultdict(list)
 for i in range(6):
-    paper[i] = [" "]*50
+    paper[i] = [" "] * 50
 for i, dot in enumerate(dots):
     paper[dot[1]][dot[0]] = "#"
 for p in paper.values():
