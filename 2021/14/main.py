d748456d6e4c74990c2b88d589e6fe23a9038004
Alfred Gamulo <alfred.gamulo@gmail.com>
2021-12-14 10:48:36 -0500

    day 14 messy

--- 2021/14/main.py
+++ 2021/14/main.py
@@ -12,9 +12,34 @@ for _ in range(10):
     for a, b in zip(polymer, polymer[1:]):
         new_poly += rules[a + b] + b
     polymer = new_poly
+    pairs = defaultdict(int)
+    for a,b in zip(polymer,polymer[1:]):
+        pairs[a+b] += 1
 
 counter = defaultdict(int)
 for p in polymer:
     counter[p] += 1
 
 print("Part 1:", max(counter.values()) - min(counter.values()))
+
+# try something messy clean up later
+polymer = templ
+pairs = defaultdict(int)
+for a,b in zip(polymer,polymer[1:]):
+    pairs[a+b] += 1
+
+for _ in range(40):
+    newpairs = defaultdict(int)
+    for k,v in pairs.items():
+        if v > 0:
+            newpairs[k[0]+rules[k]] += v-1 + 1
+            newpairs[rules[k]+k[1]] += v -1 + 1
+    
+    pairs = newpairs
+
+counter = defaultdict(int)
+for k,v in pairs.items():
+    counter[k[0]] += v
+    counter[k[1]] += v
+
+print("Part 2:", (max(counter.values()) - min(counter.values()) + 1 )//2)
