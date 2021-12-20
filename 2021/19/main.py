679859c03fe123acd2c97d6b683672469cf39dbe
Alfred Gamulo <alfred.gamulo@gmail.com>
2021-12-20 16:31:53 -0500

    :(

--- 2021/19/main.py
+++ 2021/19/main.py
@@ -16,23 +16,183 @@ for i, line in enumerate(lines):
 # pp.pprint(scanner_data)
 
 
-probe_difference_map = defaultdict(dict)
-for s, probes in scanner_data.items():
-    for i, j in permutations(range(len(probes)), 2):
-        x, y, z = (
-            abs(probes[i][0] - probes[j][0]),
-            abs(probes[i][1] - probes[j][1]),
-            abs(probes[i][2] - probes[j][2]),
-        )
-        probe_difference_map[s][(x, y, z)] = (i, j)
-# pp.pprint(probe_difference_map[0])
+"""
 
-scanner_probe_intersection = defaultdict(dict)
-for i, j in combinations(range(len(scanner_data)+1), 2):
-    common_probes = set(probe_difference_map[i].keys()).intersection(
-        probe_difference_map[j].keys()
-    )
-    if common_probes:
-        scanner_probe_intersection[(i, j)] = common_probes
+z   y
+|  /
+| /
+|/_____ x
 
-# pp.pprint(scanner_probe_intersection)
+y   -z
+|  /
+| /
+|/_____ x
+
+-z   -y
+|  /
+| /
+|/_____ x
+
+-y  z
+|  /
+| /
+|/_____ x
+
+*
+
+z   -y
+|  /
+| /
+|/_____ -x
+
+-y   -z
+|  /
+| /
+|/_____ -x
+
+-z   y
+|  /
+| /
+|/_____ -x
+
+y   z
+|  /
+| /
+|/_____ -x
+
+* 
+
+z   -x
+|  /
+| /
+|/_____ y
+
+-x   -z
+|  /
+| /
+|/_____ y
+
+-z   x
+|  /
+| /
+|/_____ y
+
+x   z
+|  /
+| /
+|/_____ y
+
+*
+
+z   x
+|  /
+| /
+|/_____ -y
+
+x   -z
+|  /
+| /
+|/_____ -y
+
+-z   -x
+|  /
+| /
+|/_____ -y
+
+-x   z
+|  /
+| /
+|/_____ -y
+
+*
+
+-x   y
+|  /
+| /
+|/_____ z
+
+y   x
+|  /
+| /
+|/_____ z
+
+x   -y
+|  /
+| /
+|/_____ z
+
+-y  -x
+|  /
+| /
+|/_____ z
+
+*
+
+-y  x
+|  /
+| /
+|/_____ -z
+
+x  y
+|  /
+| /
+|/_____ -z
+
+y  -x
+|  /
+| /
+|/_____ -z
+
+-x  -y
+|  /
+| /
+|/_____ -z
+
+"""
+
+rotation_options = {
+    0: lambda x, y, z: (x, y, z),
+    1: lambda x, y, z: (x, -z, y),
+    2: lambda x, y, z: (x, -y, -z),
+    3: lambda x, y, z: (x, z, -y),
+    4: lambda x, y, z: (-x, -y, z),
+    5: lambda x, y, z: (-x, -z, -y),
+    6: lambda x, y, z: (-x, y, -z),
+    7: lambda x, y, z: (-x, z, y),
+    8: lambda x, y, z: (y, -x, z),
+    9: lambda x, y, z: (y, -z, -x),
+    10: lambda x, y, z: (y, x, -z),
+    11: lambda x, y, z: (y, z, x),
+    12: lambda x, y, z: (-y, x, z),
+    13: lambda x, y, z: (-y, -z, x),
+    14: lambda x, y, z: (-y, -x, -z),
+    15: lambda x, y, z: (-y, z, -x),
+    16: lambda x, y, z: (z, y, -x),
+    17: lambda x, y, z: (z, x, y),
+    18: lambda x, y, z: (z, -y, x),
+    19: lambda x, y, z: (z, -x, -y),
+    20: lambda x, y, z: (-z, x, -y),
+    21: lambda x, y, z: (-z, y, x),
+    22: lambda x, y, z: (-z, -x, y),
+    23: lambda x, y, z: (-z, -y, -x),
+}
+
+def find_scanner_match(scanner1, scanner2):
+    for a, p in scanner_data[scanner1].items():
+        for o in rotation_options:
+            x_diff_counter = defaultdict(int)
+            y_diff_counter = defaultdict(int)
+            z_diff_counter = defaultdict(int)
+            for b, q in scanner_data[scanner2].items():
+                xx, yy, zz = rotation_options[o](q[0],q[1],q[2])
+                x_diff_counter[p[0]-xx] += 1
+                y_diff_counter[p[1]-yy] += 1
+                z_diff_counter[p[2]-zz] += 1
+            if max(x_diff_counter.values()) >= 12 and max(y_diff_counter.values()) >= 12 and max(z_diff_counter.values()) >= 12:
+                print("!!!!!!!!!!!!")
+                return (scanner1, scanner2, o)
+
+for i in range(1, len(scanner_data)):
+    print(find_scanner_match(0,i))
+
+
