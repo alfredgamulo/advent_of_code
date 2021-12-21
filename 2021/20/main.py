bdd4930412c42bb58163aba18c30b401658c6576
Alfred Gamulo <alfred.gamulo@gmail.com>
2021-12-21 10:57:54 -0500

    checkpoint

--- 2021/20/main.py
+++ 2021/20/main.py
@@ -2,6 +2,7 @@ import sys
 from itertools import product
 
 # 5179
+# 16112
 algorithm, image_string = sys.stdin.read().split("\n\n")
 image_string = [list(s) for s in image_string.strip().split()]
 lenx = len(image_string)
@@ -25,7 +26,7 @@ for i in range(len(image_string)):
         if image_string[i][j] == "#":
             image.add((i + 1, j + 1))
 
-toggle = -1
+toggle = 0
 for _ in range(2):
     output = set()
     lenx += 2
@@ -34,14 +35,15 @@ for _ in range(2):
         b = 0
         for e in range(len(bit_lookup)):
             x, y = i + bit_lookup[e][0], j + bit_lookup[e][1]
-            if (x, y) in image or (
+            if (x, y) in image  or (
                 algorithm[0] == "#"
-                and (toggle:= ~toggle)
-                and (x <= 0 or x >= lenx  or y <= 0 or y >= leny)
+                and toggle
+                and (x < 0 or x > lenx  or y < 0 or y > leny)
             ):
                 b += 1 << e
         if algorithm[b] == "#":
             output.add((i + 1, j + 1))
     image = output
+    toggle = not toggle
 
 print("Part 1:", len(image))
