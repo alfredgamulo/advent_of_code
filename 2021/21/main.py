6f14a4d9db852af21b1871cf77895cbc25886d70
Alfred Gamulo <alfred.gamulo@gmail.com>
2021-12-21 21:56:11 -0500

    lint

--- 2021/21/main.py
+++ 2021/21/main.py
@@ -2,7 +2,7 @@ import sys
 from functools import cache
 from itertools import product
 
-player1, player2 = map(lambda s: int(s.split(":")[-1].strip()), sys.stdin.readlines())
+p1, p2 = map(lambda s: int(s.split(":")[-1].strip()), sys.stdin.readlines())
 
 
 def die(sides, counter):
@@ -13,42 +13,44 @@ def die(sides, counter):
         yield number
 
 
-def part1(player1, player2):
+def part1(p1, p2):
     roll_counter = [0]
     roll = die(100, roll_counter)
-    score1 = 0
-    score2 = 0
+    s1 = 0
+    s2 = 0
     while True:
         spaces = next(roll) + next(roll) + next(roll)
-        player1 = (player1 + spaces - 1) % 10 + 1
-        score1 += player1
-        if score1 >= 1000:
+        p1 = (p1 + spaces - 1) % 10 + 1
+        s1 += p1
+        if s1 >= 1000:
             break
 
         spaces = next(roll) + next(roll) + next(roll)
-        player2 = (player2 + spaces - 1) % 10 + 1
-        score2 += player2
-        if score2 >= 1000:
+        p2 = (p2 + spaces - 1) % 10 + 1
+        s2 += p2
+        if s2 >= 1000:
             break
-    return min(score1, score2) * roll_counter[0]
+    return min(s1, s2) * roll_counter[0]
 
 
-print("Part 1:", part1(player1, player2))
+print("Part 1:", part1(p1, p2))
 
-quantum_moves = [sum(rolls) for rolls in (product([1,2,3], repeat=3))]
+quantum_moves = [sum(rolls) for rolls in (product([1, 2, 3], repeat=3))]
+
 
 @cache
-def part2(player1, score1, player2, score2, turn=True):
-    if score1 >= 21:
+def part2(p1, s1, p2, s2, turn=True):
+    if s1 >= 21:
         return 1
-    if score2 >= 21:
+    if s2 >= 21:
         return 0
     if turn:
-        moves = [(player1 + moves - 1) % 10 + 1 for moves in quantum_moves]
-        results = (part2(move, score1 + move, player2, score2, not turn) for move in moves)
+        moves = [(p1 + moves - 1) % 10 + 1 for moves in quantum_moves]
+        results = (part2(move, s1 + move, p2, s2, not turn) for move in moves)
     else:
-        moves = [(player2 + moves - 1) % 10 + 1 for moves in quantum_moves]
-        results = (part2(player1, score1, move, score2 + move, not turn) for move in moves)
+        moves = [(p2 + moves - 1) % 10 + 1 for moves in quantum_moves]
+        results = (part2(p1, s1, move, s2 + move, not turn) for move in moves)
     return sum(results)
 
-print("Part 2:", part2(player1, 0, player2, 0))
+
+print("Part 2:", part2(p1, 0, p2, 0))
