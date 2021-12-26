ecbfdc50c7b3d0db58620bd9e567d3150204643a
Alfred Gamulo <alfred.gamulo@gmail.com>
2021-12-25 20:45:32 -0500

    tider

--- 2021/24/main.py
+++ 2021/24/main.py
@@ -5,42 +5,42 @@ This day does is not a general solution for any input, but a decompilation of my
 
 def solver(part1):
     if part1:
-        r = list(range(9, 0, -1))
+        r = range(9, 0, -1)
     else:
-        r = list(range(1, 10))
+        r = range(1, 10)
     model = [0,0,0,0,0,0,0,0,0,0,0,0,0,0]
     for i in r:
-        if i + 9 - 2 in range(1, 10):
+        if i + 9 - 2 in r:
             model[0] = i
             model[13] = i + 9 - 2
             break
     for i in r:
-        if i + 1 - 4 in range(1, 10):
+        if i + 1 - 4 in r:
             model[1] = i
             model[12] = i + 1 - 4
             break
     for i in r:
-        if i + 11 - 16 in range(1, 10):
+        if i + 11 - 16 in r:
             model[2] = i
             model[11] = i + 11 - 16
             break
     for i in r:
-        if i + 3 - 11 in range(1, 10):
+        if i + 3 - 11 in r:
             model[3] = i
             model[4] = i + 3 - 11
             break
     for i in r:
-        if i + 5 - 6 in range(1, 10):
+        if i + 5 - 6 in r:
             model[5] = i
             model[10] = i + 5 - 6
             break
     for i in r:
-        if i + 0 - 6 in range(1, 10):
+        if i + 0 - 6 in r:
             model[6] = i
             model[7] = i + 0 - 6
             break
     for i in r:
-        if i + 9 - 6 in range(1, 10):
+        if i + 9 - 6 in r:
             model[8] = i
             model[9] = i + 9 - 6
             break
