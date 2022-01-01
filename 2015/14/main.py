4c6c04a047f32219edd4d9262427355669548d5f
Alfred Gamulo <alfred.gamulo@gmail.com>
2021-12-31 22:16:19 -0500

    2015/14

--- 2015/14/main.py
+++ 2015/14/main.py
@@ -1,17 +1,36 @@
 import sys
+from collections import defaultdict
 
 lines = sys.stdin.readlines()
 
-flights = dict()
+reindeer = dict()
 for line in lines:
     name, _, _, speed, _, _, duration, *_, rest, _ = line.split()
-    flights[name] = (int(speed), int(duration), int(rest))
+    reindeer[name] = (int(speed), int(duration), int(rest))
 
 distance = -1
-for reindeer, stats in flights.items():
+for stats in reindeer.values():
     i, m = divmod(2503, (stats[1] + stats[2]))
     t = min(stats[1], m)
     d = (i * stats[1] + t) * stats[0]
     distance = max(distance, d)
 
 print("Part 1:", distance)
+
+race = defaultdict(dict)
+lead = -1
+top = 0
+for t in range(2503):
+    for r, stats in reindeer.items():
+        if r not in race:
+            race[r] = defaultdict(int)
+        d, m = divmod(t, stats[1] + stats[2])
+        if t < stats[1] or (d and m < stats[1]):
+            race[r]["distance"] += stats[0]
+        lead = max(lead, race[r]["distance"])
+    for r in race:
+        if race[r]["distance"] == lead:
+            race[r]["score"] += 1
+            top = max(top, race[r]["score"])
+
+print("Part 2:", top)
