cacae7dcee6238db18bcf72b1891c0d26ade9112
Alfred Gamulo <alfred.gamulo@gmail.com>
2022-01-01 00:56:58 -0500

    2015/15

---
+++ 2015/15/main.py
@@ -0,0 +1,55 @@
+import sys
+from collections import defaultdict
+from itertools import  permutations
+from pprint import PrettyPrinter
+
+pp = PrettyPrinter()
+
+lines = sys.stdin.readlines()
+
+ingredients = defaultdict(dict)
+for line in lines:
+    line = line.strip()
+    name, _, capacity, _, durability, _, flavor, _, texture, _, calories = line.split()
+    name = name[:-1]
+    capacity = int(capacity[:-1])
+    durability = int(durability[:-1])
+    flavor = int(flavor[:-1])
+    texture = int(texture[:-1])
+    calories = int(calories)
+    ingredients[name] = {
+        "capacity": capacity,
+        "durability": durability,
+        "flavor": flavor,
+        "texture": texture,
+        "calories": calories
+    }
+
+pp.pprint(ingredients)
+
+def get_highscore(check_calories=False):
+    highscore = set()
+    for p in permutations(range(1, 101), len(ingredients)):
+        if sum(p) != 100:
+            continue
+        capacity = 0
+        durability = 0
+        flavor = 0
+        texture = 0
+        calories = 0
+        for i, ingredient in enumerate(ingredients):
+            capacity += ingredients[ingredient]["capacity"]*p[i]
+            durability += ingredients[ingredient]["durability"]*p[i]
+            flavor += ingredients[ingredient]["flavor"]*p[i]
+            texture += ingredients[ingredient]["texture"]*p[i]
+            calories += ingredients[ingredient]["calories"]*p[i]
+        if capacity < 0 or durability < 0 or flavor < 0 or texture < 0:
+            continue
+        if check_calories and calories != check_calories:
+            continue
+        highscore.add(capacity*durability*flavor*texture)
+        
+    return max(highscore)
+
+print("Part 1:", get_highscore())
+print("Part 2:", get_highscore(374)) # 11171160
