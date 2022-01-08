68d26617f8d82ef3dd71f75afd121099ddb39147
Alfred Gamulo <alfred.gamulo@gmail.com>
2022-01-07 21:35:52 -0500

    2015/17

---
+++ 2015/17/main.py
@@ -0,0 +1,14 @@
+import sys
+from itertools import combinations
+from collections import defaultdict
+
+containers = list(map(int, sys.stdin.readlines()))
+
+combos = defaultdict(int)
+for r in range(len(containers)):
+    for combo in  combinations(containers, r):
+        if sum(combo) == 150:
+            combos[len(combo)] += 1
+
+print("Part 1:", sum(combos.values()))
+print("Part 2:", combos[min(combos)])
