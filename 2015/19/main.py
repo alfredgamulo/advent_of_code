7e7eccc800fcc6a2ced0e4c7485894b8cabadf8c
Alfred Gamulo <alfred.gamulo@gmail.com>
2022-01-09 20:45:33 -0500

    2015/19

--- 2015/19/main.py
+++ 2015/19/main.py
@@ -29,16 +29,12 @@ print("Part 1:", part1())
 
 
 def part2():
-    molecules = set()
-    for k, V in replacements.items():
-        for v in V:
-            for finding in re.finditer(k, calibration):
-                molecules.add(
-                    calibration[: finding.span()[0]]
-                    + v
-                    + calibration[finding.span()[1] :]
-                )
-    return len(molecules)
+    global calibration
+    c = calibration[:]
+    c = str.replace(c, "Rn", "(")
+    c = str.replace(c, "Y", ",")
+    c = str.replace(c, "Ar", ")")
+    return sum(map(str.isupper, c)) - sum(map(lambda x: x == ",", c)) - 1
 
 
 print("Part 1:", part2())
