9cfdb9dd2d882c3f2aefd8d2566e6238b66800e6
Alfred Gamulo <alfred.gamulo@gmail.com>
2022-04-26 21:02:47 -0400

    2017/07 checkpoint

---
+++ 2017/07/main.py
@@ -0,0 +1,16 @@
+import sys
+
+
+def part1(lines):
+    print(lines)
+
+
+def part2():
+    pass
+
+
+if __name__ == "__main__":
+    lines = sys.stdin.readlines()
+
+    print("Part 1:", part1(lines))
+    print("Part 2:", part2())
