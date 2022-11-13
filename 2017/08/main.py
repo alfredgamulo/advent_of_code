a2e0cd435f22a661fcada7e33758a0a22e4aae99
Alfred Gamulo <alfred.gamulo@gmail.com>
2022-11-13 18:10:48 -0500

    2017/08 solved

--- 2017/08/main.py
+++ 2017/08/main.py
@@ -1,10 +1,12 @@
 import sys
 
 registers = {}
+mval = 0
 
 
-def part1(lines):
-    global registers
+if __name__ == "__main__":
+    lines = sys.stdin.readlines()
+
     for line in lines:
         r1, op, a1, _, r2, cond, a2 = line.split()
         r2 = registers.get(r2, 0)
@@ -14,16 +16,6 @@ def part1(lines):
                 registers[r1] = registers.get(r1, 0) + int(a1)
             else:
                 registers[r1] = registers.get(r1, 0) - int(a1)
+            mval = max(registers[r1], mval)
     largest = max(registers, key=registers.get)
-    return largest, registers.get(largest)
-
-
-def part2():
-    pass
-
-
-if __name__ == "__main__":
-    lines = sys.stdin.readlines()
-
-    print("Part 1:", part1(lines))
-    print("Part 2:", part2())
+    print(largest, registers.get(largest), mval)
