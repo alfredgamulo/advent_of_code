0937a0dbe8cf6879ff0e8cb2af2552d77e9cccfc
Alfred Gamulo <alfred.gamulo@gmail.com>
2022-11-18 19:05:43 -0500

    2017/19 done

--- 2017/19/main.py
+++ 2017/19/main.py
@@ -1,18 +1,15 @@
 import sys
 from collections import deque
 from itertools import cycle
+from string import ascii_uppercase
 
 
-def part1(lines):
-    print(list(lines))
-
+def solve(lines):
     # populate the network
     network = {}
     for row, line in enumerate(lines):
         for column, character in enumerate(line[:-1]):
-
             if character != " ":
-                print(row, column, character)
                 network[(row, column)] = character
 
     # find the root
@@ -21,27 +18,41 @@ def part1(lines):
         if network.get((0, c)) == "|":
             position = (0, c)
 
-    adjs = {"cols": ((0, -1), (0, 1)), "rows": ((-1, 0), (1, 0))}
-    lins = {"cols": "|", "rows": "-"}
-    toggle = cycle(("rows", "cols"))
+    adjs = {"horz": ((0, -1), (0, 1)), "vert": ((-1, 0), (1, 0))}
+    lins = {"vert": "|", "horz": "-"}
+    toggle = cycle(("vert", "horz"))
     direction = next(toggle)
-    print(direction)
 
+    # Traverse
     stack = deque([position])
-    visited = set()
+    count = 0
     letters = []
+    x = 1
+    y = 0
     while stack:
         pos = stack.popleft()
-        visited.add(pos)
-        pass
+        count += 1
+        if network[pos] in ascii_uppercase:
+            letters.append(network[pos])
+        if network[pos] != "+":
+            if (pos[0] + x, pos[1] + y) in network:
+                stack.append((pos[0] + x, pos[1] + y))
+                continue
+        else:
+            direction = next(toggle)
+            for (i, j) in adjs[direction]:
+                if (pos[0] + i, pos[1] + j) in network and network[
+                    (pos[0] + i, pos[1] + j)
+                ] == lins[direction]:
+                    x = i
+                    y = j
+                    stack.append((pos[0] + i, pos[1] + j))
 
-
-def part2():
-    pass
+    print("Part 1:", "".join(letters))
+    print("Part 2:", count)
 
 
 if __name__ == "__main__":
     lines = sys.stdin.readlines()
 
-    print("Part 1:", part1(lines))
-    print("Part 2:", part2())
+    solve(lines)
