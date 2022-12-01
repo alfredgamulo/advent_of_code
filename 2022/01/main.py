4c3d14bea79582b07a83f73e56cfd8fbd72d2aa8
Alfred Gamulo <alfred.gamulo@gmail.com>
2022-12-01 09:43:26 -0500

    2022/01 done

---
+++ 2022/01/main.py
@@ -0,0 +1,22 @@
+import sys
+
+
+def part1(inventory):
+    most = 0
+    for i in inventory:
+        most = max(most, sum(i))
+    return most
+
+
+def part2(inventory):
+    sorted_inventory = sorted((sum(i) for i in inventory), reverse=True)
+    return sum(sorted_inventory[:3])
+
+
+if __name__ == "__main__":
+    lines = sys.stdin.read().strip()
+    inventory = lines.split("\n\n")
+    inventory = [list(map(int, i.split("\n"))) for i in inventory if i]
+
+    print("Part 1:", part1(inventory))
+    print("Part 2:", part2(inventory))
