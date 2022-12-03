cb0129383488b34d6dea3ba2758bbd04be5ec127
Alfred Gamulo <alfred.gamulo@gmail.com>
2022-12-02 23:59:38 -0500

    feat: go read from stdin

--- common/template.go
+++ common/template.go
@@ -1,7 +1,17 @@
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
 
 func main() {
-	fmt.Println("Hello, World!")
+	scanner := bufio.NewScanner(os.Stdin)
+	for scanner.Scan() {
+		fmt.Println(scanner.Text()) // Println will add back the final '\n'
+	}
+	if err := scanner.Err(); err != nil {
+		fmt.Fprintln(os.Stderr, "reading standard input:", err)
+	}
 }
