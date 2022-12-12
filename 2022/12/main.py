23ad1870f12e3ce9d6b7e24b702ca22a87541870
Alfred Gamulo <alfred.gamulo@gmail.com>
2022-12-12 15:46:20 -0500

    2022/12 done

--- 2022/12/main.py
+++ 2022/12/main.py
@@ -3,31 +3,26 @@ from collections import deque
 from itertools import product
 
 
-def get_val(graph, tup):
-    return graph[tup[0]][tup[1]]
-
-
 def solve(graph, start, end):
     graphcoords = set(product(range(len(graph)), range(len(graph[0]))))
-    for coords in graphcoords:
-        if start == graph[coords[0]][coords[1]]:
-            s = coords
-    adjacencies = ((-1, 0), (1, 0), (0, -1), (0, 1))
-    visited = set([s])
-    moveque = deque([[s]])
-    graph[s[0]] = graph[s[0]].replace("E", "z")
+    for (r, c) in graphcoords:
+        if start == graph[r][c]:
+            visited = set([(r, c)])
+            moveque = deque([[(r, c)]])
+            graph[r] = graph[r].replace("E", "z")
     while moveque:
         points = moveque.popleft()
-        for a in adjacencies:
-            new = (points[-1][0] + a[0], points[-1][1] + a[1])
-            if new in graphcoords and new not in visited:
-                if get_val(graph, new) == end:
-                    if get_val(graph, points[-1]) in ("b", "a"):
+        r, c = points[-1]
+        for i, j in ((-1, 0), (1, 0), (0, -1), (0, 1)):
+            x, y = (r + i, c + j)
+            if (x, y) in graphcoords and (x, y) not in visited:
+                if graph[x][y] == end:
+                    if graph[r][c] in ("b", "a"):
                         return len(points)
                 else:
-                    if ord(get_val(graph, points[-1])) - ord(get_val(graph, new)) <= 1:
-                        visited.add(new)
-                        moveque.append(points[:] + [new])
+                    if ord(graph[r][c]) - ord(graph[x][y]) <= 1:
+                        visited.add((x, y))
+                        moveque.append(points[:] + [(x, y)])
 
 
 if __name__ == "__main__":
