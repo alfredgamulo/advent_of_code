5c83447cec073e08c1aae10788454c292fcb6909
Alfred Gamulo <alfred.gamulo@gmail.com>
2022-12-18 07:57:30 -0500

    2022/17 checkpoint

--- 2022/17/main.py
+++ 2022/17/main.py
@@ -1,40 +1,6 @@
 import sys
+from collections import defaultdict, deque
 from itertools import cycle
-
-"""
-shapes:
-
-####
-
-.#.
-###
-.#.
-
-..#
-..#
-###
-
-#
-#
-#
-#
-
-##
-##
-
-"""
-
-"""
-seven-wide
-
-2 to the right
-3 up
-|..@@@@.|
-|.......|
-|.......|
-|.......|
-+-------+
-"""
 
 
 def shapes():
@@ -45,8 +11,8 @@ def shapes():
         [(2, 3), (2, 4), (2, 5), (2, 6)],
         [(2, 3), (3, 3), (2, 4), (3, 4)],
     ]
-    for s in cycle(shapes):
-        yield s
+    for i, s in cycle(enumerate(shapes)):
+        yield i, s
 
 
 def jets(pattern):
@@ -54,8 +20,8 @@ def jets(pattern):
         ">": (1, 0),
         "<": (-1, 0),
     }
-    for j in cycle(pattern):
-        yield lookup[j]
+    for i, j in cycle(enumerate(pattern)):
+        yield i, lookup[j]
 
 
 def shift(shape, offset):
@@ -65,16 +31,15 @@ def shift(shape, offset):
     return res
 
 
-def part1(lines):
-    shape = shapes()
-    jet = jets(lines[0])
+def solve(iterations, jet, shape):
     floor = 0
-    formation = set()
-    for _ in range(2022):
-        rock = shift(next(shape), (0, floor))
+    formation = deque(maxlen=1000)
+
+    for _ in range(iterations):
+        rock = shift(next(shape)[1], (0, floor))
         stuck = False
         while not stuck:
-            moved_rock = shift(rock, next(jet))
+            moved_rock = shift(rock, next(jet)[1])
             if all(0 <= x < 7 and (x, y) not in formation for x, y in moved_rock):
                 rock = moved_rock
             moved_rock = shift(rock, (0, -1))
@@ -82,17 +47,75 @@ def part1(lines):
                 rock = moved_rock
             else:
                 stuck = True
-        formation.update(rock)
-        floor = max(y for _, y in rock) + 1
-    return floor - 1
+        formation.extend(rock)
+        floor = max(floor, max(y for _, y in rock) + 1)
+
+    return floor
 
 
-def part2():
-    pass
+def part1(lines):
+    jet = jets(lines[0])
+    shape = shapes()
+    return solve(2022, jet, shape)
 
+
+def movement(rock, init):
+    res = []
+    for r, i in zip(rock, init):
+        res.append((r[0] - i[0], r[1] - i[1]))
+    return tuple(res)
+
+
+def part2(lines):
+    # 1000000000000
+    jet = jets(lines[0])
+    shape = shapes()
+
+    floor = 0
+    formation = deque(maxlen=1000)
+
+    cycles = None
+    cache = defaultdict(list)
+    history = deque(maxlen=len(lines))
+    for i in range(9000):
+        # print(i, flush=True)
+        si, sn = next(shape)
+        rock = shift(sn, (0, floor))
+        init = rock
+        stuck = False
+        while not stuck:
+            ji, jn = next(jet)
+            moved_rock = shift(rock, jn)
+            if all(0 <= x < 7 and (x, y) not in formation for x, y in moved_rock):
+                rock = moved_rock
+            moved_rock = shift(rock, (0, -1))
+            if all(y >= 0 and (x, y) not in formation for x, y in moved_rock):
+                rock = moved_rock
+            else:
+                stuck = True
+        formation.extend(rock)
+        floor = max(floor, max(y for _, y in rock) + 1)
+
+        history.append(movement(rock, init))
+        cache[(si, ji, tuple(history))].append((i, floor - 1))
+        if len(cache[(si, ji, tuple(history))]) > 5:
+            cycles = (
+                cache[(si, ji, tuple(history))][1],
+                cache[(si, ji, tuple(history))][3],
+            )
+            break
+
+    i1, h1 = cycles[0]
+    i2, h2 = cycles[1]
+
+    # return ((1000000000000 - i1) // (i2 - i1) * (h2 - h1)) + h1
+
+
+# too low 1540634005750
+#         1540634005764 < not right
 
 if __name__ == "__main__":
     lines = sys.stdin.read().splitlines()
 
     print("Part 1:", part1(lines))
-    print("Part 2:", part2())
+    print("Part 2:", part2(lines))  # looking for 1514285714288
