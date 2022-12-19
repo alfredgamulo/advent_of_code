446d96d68f15c226a7ab4abc80210c3a2f4864a3
Alfred Gamulo <alfred.gamulo@gmail.com>
2022-12-18 21:57:55 -0500

    2022/18 done

--- 2022/18/main.py
+++ 2022/18/main.py
@@ -1,4 +1,5 @@
 import sys
+from collections import deque
 
 adj = set([(-1, 0, 0), (1, 0, 0), (0, -1, 0), (0, 1, 0), (0, 0, -1), (0, 0, 1)])
 
@@ -8,16 +9,31 @@ def part1(coords):
 
 
 def part2(coords):
-    surfaces = set()
+    max_x, max_y, max_z = 0, 0, 0
     for c in coords:
+        max_x, max_y, max_z = (
+            max(max_x, c[0] + 1),
+            max(max_y, c[1] + 1),
+            max(max_z, c[2] + 1),
+        )
+
+    stack = deque([(0, 0, 0)])
+    visit = set()
+    res = 0
+    while stack:
+        current = stack.popleft()
         for a in adj:
-            n = tuple(map(sum, zip(c, a)))
-            if n not in coords:
-                surfaces.add(n)
-
-    bubbles = 0
-
-    return len(surfaces) - bubbles
+            if (p := tuple(map(sum, zip(current, a)))) in coords:
+                res += 1
+            elif (
+                p not in visit
+                and (-1 <= p[0] <= max_x)
+                and (-1 <= p[1] <= max_y)
+                and (-1 <= p[2] <= max_z)
+            ):
+                stack.append(p)
+                visit.add(p)
+    return res
 
 
 if __name__ == "__main__":
