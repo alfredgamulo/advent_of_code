c6589dce06bee2fb3ce8bae854283c66d31128c4
Alfred Gamulo <alfred.gamulo@gmail.com>
2023-10-23 14:40:11 -0400

    feat: 2018/12 done

---
+++ 2018/12/main.py
@@ -0,0 +1,43 @@
+import sys
+from collections import deque
+from itertools import chain
+
+
+def solve(pots, rules, rounds):
+    for i in range(rounds):
+        new_pots = set()
+        buffer = deque(".....")
+        for p in range(min(pots) - 2, max(pots) + 3):
+            buffer.popleft()
+            if (p + 2) in pots:
+                buffer.append("#")
+            else:
+                buffer.append(".")
+
+            if "".join(buffer) in rules:
+                new_pots.add(p)
+        print(i + 1, "\t", sum(new_pots), "\tdiff:", sum(new_pots) - sum(pots))
+        pots = new_pots
+
+    return sum(pots)
+
+
+def part2():
+    pass
+
+
+if __name__ == "__main__":
+    sections = sys.stdin.read().split("\n\n")
+
+    initial_state = sections[0].split(": ")[1]
+    pots = set([i for i, p in enumerate(initial_state) if p == "#"])
+
+    rules = set()
+    for r in sections[1].splitlines():
+        k, v = r.split(" => ")
+        if v == "#":
+            rules.add(k)
+
+    print("Part 1:", solve(pots, rules, 200))
+    # Stabilizes, check diffs in output
+    print("Part 2:", 10390 + 73 * (50000000000 - 118))
