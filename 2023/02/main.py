a31177ca7f9c8b2c1529ad67b9f7bbe80fb44f2e
Alfred Gamulo <alfred.gamulo@gmail.com>
2023-12-02 08:14:14 -0500

    further cute

--- 2023/02/main.py
+++ 2023/02/main.py
@@ -3,28 +3,27 @@ from collections import defaultdict
 from math import prod
 
 
-def solve(handfuls):
+def solve(games):
+    possibles, power_sum = 0, 0
     rules = {"red": 12, "green": 13, "blue": 14}
-    maxes = defaultdict(int)
-    for handful in handfuls.split(";"):
-        for cubes in handful.split(","):
-            amount, color = cubes.split()
-            maxes[color] = max(int(amount), maxes[color])
-    part1 = all(a >= maxes[c] for c, a in rules.items())
-    part2 = prod(maxes.values())
-    return part1, part2
+
+    for id, game in enumerate(games, 1):
+        maxes = defaultdict(int)
+        for handful in game.split(":")[1].split(";"):
+            for cubes in handful.split(","):
+                amount, color = cubes.split()
+                maxes[color] = max(int(amount), maxes[color])
+
+        possibles += all(a >= maxes[c] for c, a in rules.items()) * id
+        power_sum += prod(maxes.values())
+
+    return possibles, power_sum
 
 
 if __name__ == "__main__":
     games = sys.stdin.read().splitlines()
 
-    answer1 = 0
-    answer2 = 0
-    for id, game in enumerate(games, 1):
-        handfuls = game.split(":")[1]
-        part1, part2 = solve(handfuls)
-        answer1 += part1 * id
-        answer2 += part2
+    part1, part2 = solve(games)
 
-    print("Part 1:", answer1)
-    print("Part 2:", answer2)
+    print("Part 1:", part1)
+    print("Part 2:", part2)
