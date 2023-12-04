5133d90397aad03f88b3904c294dda1148c1bb28
Alfred Gamulo <alfred.gamulo@gmail.com>
2023-12-04 12:08:57 -0500

    feat: make it cuter

--- 2023/04/main.py
+++ 2023/04/main.py
@@ -5,28 +5,23 @@ from collections import defaultdict
 
 def solve(cards):
     part1 = 0
-    part2 = 0
     multipliers = defaultdict(int)
-    for id, card in enumerate(cards, 1):
-        if matches := len(card[0].intersection(card[1])):
+    for id, numbers in enumerate(cards, 1):
+        if matches := len(next(numbers).intersection(next(numbers))):
             part1 += 2 ** (matches - 1)
-        instances = multipliers[id] + 1
-        part2 += instances
         for future in range(1, matches + 1):
-            multipliers[id + future] += instances
-    return part1, part2
+            multipliers[id + future] += multipliers[id] + 1
+    return part1, sum(multipliers.values()) + len(cards)
 
 
 if __name__ == "__main__":
-    cards = list(
-        list(
-            map(
-                lambda card: set(re.findall("(\\d+)", card)),
-                line.split(":")[1].split("|"),
-            )
+    cards = [
+        map(
+            lambda card: set(re.findall("(\\d+)", card)),
+            line.split(":")[1].split("|"),
         )
         for line in sys.stdin.read().splitlines()
-    )
+    ]
     part1, part2 = solve(cards)
     print("Part 1:", part1)
     print("Part 2:", part2)
