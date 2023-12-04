9530a299af60b08a7408d2f43b3a342d3973dd07
Alfred Gamulo <alfred.gamulo@gmail.com>
2023-12-04 12:12:09 -0500

    cuter

--- 2023/04/main.py
+++ 2023/04/main.py
@@ -5,13 +5,16 @@ from collections import defaultdict
 
 def solve(cards):
     part1 = 0
+    part2 = 0
     multipliers = defaultdict(int)
     for id, numbers in enumerate(cards, 1):
         if matches := len(next(numbers).intersection(next(numbers))):
             part1 += 2 ** (matches - 1)
+        part2 += (instances := multipliers[id] + 1)
         for future in range(1, matches + 1):
-            multipliers[id + future] += multipliers[id] + 1
-    return part1, sum(multipliers.values()) + len(cards)
+            multipliers[id + future] += instances
+
+    return part1, part2
 
 
 if __name__ == "__main__":
