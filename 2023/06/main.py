da5fcf4b6ba9c89b639c757e043dd375bc6902f3
Alfred Gamulo <alfred.gamulo@gmail.com>
2023-12-06 00:54:29 -0500

    tiniest

--- 2023/06/main.py
+++ 2023/06/main.py
@@ -1,4 +1,3 @@
-import re
 import sys
 from math import prod
 
@@ -10,19 +9,9 @@ def solve(times, dists):
     )
 
 
-def part1(lines):
-    times = map(int, re.findall("\\d+", lines[0]))
-    dists = map(int, re.findall("\\d+", lines[1]))
-    return solve(times, dists)
-
-
-def part2(lines):
-    time = int("".join(re.findall("\\d+", lines[0])))
-    dist = int("".join(re.findall("\\d+", lines[1])))
-    return solve([time], [dist])
-
-
 if __name__ == "__main__":
     lines = sys.stdin.read().splitlines()
-    print("Part 1:", part1(lines))
-    print("Part 2:", part2(lines))
+    times = lines[0].split(":")[1].split()
+    dists = lines[1].split(":")[1].split()
+    print("Part 1:", solve(map(int, times), map(int, dists)))
+    print("Part 2:", solve([int("".join(times))], [int("".join(dists))]))
