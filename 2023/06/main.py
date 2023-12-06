d34dac206c74eaca28641ddbafeadb7854a3bf87
Alfred Gamulo <alfred.gamulo@gmail.com>
2023-12-06 00:42:30 -0500

    feat: 2023/06 done

--- 2023/06/main.py
+++ 2023/06/main.py
@@ -1,30 +1,30 @@
-import copy
-import os
 import re
-import string
 import sys
-from collections import Counter, defaultdict, deque, namedtuple
-from contextlib import suppress
-from dataclasses import dataclass
-from functools import cache, cmp_to_key, reduce
-from io import StringIO
-from itertools import permutations  # noqa
-from itertools import batched, chain, combinations, count, cycle, product, zip_longest
-from math import ceil, floor, prod, sqrt
-from pathlib import Path
-from pprint import PrettyPrinter
+
+
+def solve(times, dists):
+    total = 1
+    for time, dist in zip(times, dists):
+        scores = []
+        for seconds in range(time + 1):
+            scores.append(seconds * (time - seconds))
+        total *= sum(s > dist for s in scores)
+    return total
 
 
 def part1(lines):
-    print(lines)
+    times = map(int, re.findall("\\d+", lines[0]))
+    dists = map(int, re.findall("\\d+", lines[1]))
+    return solve(times, dists)
 
 
-def part2():
-    pass
+def part2(lines):
+    time = int("".join(re.findall("\\d+", lines[0])))
+    dist = int("".join(re.findall("\\d+", lines[1])))
+    return solve([time], [dist])
 
 
 if __name__ == "__main__":
     lines = sys.stdin.read().splitlines()
-
     print("Part 1:", part1(lines))
-    print("Part 2:", part2())
+    print("Part 2:", part2(lines))
