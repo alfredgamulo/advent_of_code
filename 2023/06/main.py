195f5dac8a789a9c41307615db1b1ed21d4f2658
Alfred Gamulo <alfred.gamulo@gmail.com>
2023-12-06 00:56:12 -0500

    tiniest

--- 2023/06/main.py
+++ 2023/06/main.py
@@ -5,7 +5,7 @@ from math import prod
 def solve(times, dists):
     return prod(
         sum(seconds * (time - seconds) > dist for seconds in range(time + 1))
-        for time, dist in zip(times, dists)
+        for time, dist in zip(map(int, times), map(int, dists))
     )
 
 
@@ -13,5 +13,5 @@ if __name__ == "__main__":
     lines = sys.stdin.read().splitlines()
     times = lines[0].split(":")[1].split()
     dists = lines[1].split(":")[1].split()
-    print("Part 1:", solve(map(int, times), map(int, dists)))
-    print("Part 2:", solve([int("".join(times))], [int("".join(dists))]))
+    print("Part 1:", solve(times, dists))
+    print("Part 2:", solve(["".join(times)], ["".join(dists)]))
