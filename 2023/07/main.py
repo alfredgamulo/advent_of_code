f141f48f13562c4825a91ea86095c03383d78811
Alfred Gamulo <alfred.gamulo@gmail.com>
2023-12-07 13:02:45 -0500

    2023/07 done

---
+++ 2023/07/main.py
@@ -0,0 +1,49 @@
+import sys
+from collections import Counter
+from functools import cmp_to_key
+
+
+def cmp(left, lcount, right, rcount, order):
+    if len(lcount) == len(rcount):
+        if lcount.most_common()[0][1] == rcount.most_common()[0][1]:
+            for lchar, rchar in zip(left, right):
+                if lchar != rchar:
+                    return ((order.index(rchar) - order.index(lchar))) // abs(
+                        ((order.index(rchar) - order.index(lchar)))
+                    )
+        return (lcount.most_common()[0][1] - rcount.most_common()[0][1]) // abs(
+            (lcount.most_common()[0][1] - rcount.most_common()[0][1])
+        )
+    return (len(rcount) - len(lcount)) // abs((len(rcount) - len(lcount)))
+
+
+def cmp_wrapper1(left, right):
+    left, right = left.split()[0], right.split()[0]
+    return cmp(left, Counter(left), right, Counter(right), "AKQJT98765432")
+
+
+def cmp_wrapper2(left, right):
+    left, right = left.split()[0], right.split()[0]
+    lcount, rcount = Counter(left.replace("J", "")), Counter(right.replace("J", ""))
+    if lcount:
+        lcount[lcount.most_common(1)[0][0]] += left.count("J")
+    else:  # all J
+        lcount = Counter(left)
+    if rcount:
+        rcount[rcount.most_common(1)[0][0]] += right.count("J")
+    else:  # all J
+        rcount = Counter(right)
+    return cmp(left, lcount, right, rcount, "AKQT98765432J")
+
+
+def solve(lines, cmp):
+    return sum(
+        rank * int(line.split()[1])
+        for rank, line in enumerate(sorted(lines, key=cmp_to_key(cmp)), 1)
+    )
+
+
+if __name__ == "__main__":
+    lines = sys.stdin.read().splitlines()
+    print("Part 1:", solve(lines, cmp_wrapper1))
+    print("Part 2:", solve(lines, cmp_wrapper2))
