3d9e910f7111425fb987549e1ba517158912dc0f
Alfred Gamulo <alfred.gamulo@gmail.com>
2023-12-09 00:31:49 -0500

    2023/09 done

---
+++ 2023/09/main.py
@@ -0,0 +1,17 @@
+import sys
+from pathlib import Path
+
+
+def solve(sequence):
+    if not any(sequence):
+        return 0
+    new_sequence = list(s2 - s1 for s1, s2 in zip(sequence, sequence[1:]))
+    new_number = solve(new_sequence)
+    return sequence[-1] + new_number
+
+
+if __name__ == "__main__":
+    lines = Path(sys.argv[1]).read_text().splitlines()
+
+    print("Part 1:", sum(solve(list(map(int, line.split()))) for line in lines))
+    print("Part 2:", sum(solve(list(map(int, line.split()))[::-1]) for line in lines))
