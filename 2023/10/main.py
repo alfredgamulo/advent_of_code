c6bd813d09e44355b99e1b2bc0a42e1e34a3a87a
Alfred Gamulo <alfred.gamulo@gmail.com>
2023-12-10 14:59:02 -0500

    feat: 2023/10 done

--- 2023/10/main.py
+++ 2023/10/main.py
@@ -1,32 +1,21 @@
-import copy
-import os
-import re
-import string
 import sys
-from collections import Counter, defaultdict, deque, namedtuple
+from collections import deque
 from contextlib import suppress
-from dataclasses import dataclass
-from functools import cache, cmp_to_key, reduce
-from io import StringIO
-from itertools import permutations  # noqa
-from itertools import batched, chain, combinations, count, cycle, product, zip_longest
-from math import ceil, floor, lcm, prod, sqrt
 from pathlib import Path
 
 
 def parse(graph):
-    pipe = {}
-    loop = deque()
+    pipe, loop = set(), deque()
     for x, line in enumerate(graph):
         if (y := line.find("S")) >= 0:
-            pipe[(x, y)] = "S"
-            loop.append((x,y))
+            pipe.add((x, y))
+            loop.append((x, y))
             with suppress(IndexError):
                 l = (x, y - 1) if graph[x][y - 1] in ["-", "L", "F"] else None
                 r = (x, y + 1) if graph[x][y + 1] in ["-", "J", "7"] else None
                 u = (x - 1, y) if graph[x - 1][y] in ["|", "7", "F"] else None
                 d = (x + 1, y) if graph[x + 1][y] in ["|", "L", "J"] else None
-            loop.append(list(filter(None, [l, r, u, d]))[0]) # just need 1
+            loop.append(list(filter(None, [l, r, u, d]))[1])  # just need 1
             break
 
     neighbors = {
@@ -38,27 +27,23 @@ def parse(graph):
         "F": [(0, 1), (1, 0)],
     }
 
-    while (cursor:=loop[-1]) not in pipe:
-        pipe[cursor] = graph[cursor[0]][cursor[1]]
+    while (cursor := loop[-1]) not in pipe:
+        pipe.add(cursor)
         lookup = [
             (cursor[0] + n[0], cursor[1] + n[1])
             for n in neighbors[graph[cursor[0]][cursor[1]]]
         ]
         loop.extend([l for l in lookup if l not in pipe])
-    return pipe, loop
+    return loop
 
 
 if __name__ == "__main__":
-    graph = Path(sys.argv[1]).read_text().splitlines()
-    pipe,loop = parse(graph)
-
-    print("Part 1:", len(pipe) // 2)
-    print(len(loop))
-    # for x in range(len(graph)):
-    #     for y in range(len(graph[0])):
-    #         if pipe:=pipes.get((x,y)):
-    #             print(pipe, end="")
-    #         else:
-    #             print(".", end="")
-    #     print("\n")
-    # print("Part 2:", part2())
+    loop = parse(Path(sys.argv[1]).read_text().splitlines())
+    print("Part 1:", len(loop) // 2)
+    area = 0.0
+    for i in range(len(loop)):
+        j = (i + 1) % len(loop)
+        area += (loop[i][0] * loop[j][1]) - (loop[j][0] * loop[i][1])
+    # shoelace area then inverse Pick's theorem
+    interior = int((0.5 * abs(area)) - (len(loop) / 2) + 1)
+    print("Part 2:", interior)
