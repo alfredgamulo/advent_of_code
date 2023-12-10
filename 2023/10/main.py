ca157accd69a7b95c9de13ae0403d0488385681f
Alfred Gamulo <alfred.gamulo@gmail.com>
2023-12-10 13:58:48 -0500

    checkpoint

--- 2023/10/main.py
+++ 2023/10/main.py
@@ -15,17 +15,18 @@ from pathlib import Path
 
 
 def parse(graph):
-    stack = deque()
-    pipes = {}
+    pipe = {}
+    loop = deque()
     for x, line in enumerate(graph):
         if (y := line.find("S")) >= 0:
-            pipes[(x, y)] = "S"
+            pipe[(x, y)] = "S"
+            loop.append((x,y))
             with suppress(IndexError):
                 l = (x, y - 1) if graph[x][y - 1] in ["-", "L", "F"] else None
                 r = (x, y + 1) if graph[x][y + 1] in ["-", "J", "7"] else None
                 u = (x - 1, y) if graph[x - 1][y] in ["|", "7", "F"] else None
                 d = (x + 1, y) if graph[x + 1][y] in ["|", "L", "J"] else None
-            stack.extend(filter(None, [l, r, u, d]))
+            loop.append(list(filter(None, [l, r, u, d]))[0]) # just need 1
             break
 
     neighbors = {
@@ -37,20 +38,27 @@ def parse(graph):
         "F": [(0, 1), (1, 0)],
     }
 
-    while stack:
-        cursor = stack.popleft()
-        pipes[cursor] = graph[cursor[0]][cursor[1]]
+    while (cursor:=loop[-1]) not in pipe:
+        pipe[cursor] = graph[cursor[0]][cursor[1]]
         lookup = [
             (cursor[0] + n[0], cursor[1] + n[1])
             for n in neighbors[graph[cursor[0]][cursor[1]]]
         ]
-        stack.extend([l for l in lookup if l not in pipes])
-    return pipes
+        loop.extend([l for l in lookup if l not in pipe])
+    return pipe, loop
 
 
 if __name__ == "__main__":
     graph = Path(sys.argv[1]).read_text().splitlines()
-    pipes = parse(graph)
+    pipe,loop = parse(graph)
 
-    print("Part 1:", len(pipes) // 2)
+    print("Part 1:", len(pipe) // 2)
+    print(len(loop))
+    # for x in range(len(graph)):
+    #     for y in range(len(graph[0])):
+    #         if pipe:=pipes.get((x,y)):
+    #             print(pipe, end="")
+    #         else:
+    #             print(".", end="")
+    #     print("\n")
     # print("Part 2:", part2())
