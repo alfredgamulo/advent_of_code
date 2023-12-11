15c1e8ef9205d2f0b838d8aff38658660c85f86b
Alfred Gamulo <alfred.gamulo@gmail.com>
2023-12-11 13:11:42 -0500

    feat: 2023/11 done

--- 2023/11/main.py
+++ 2023/11/main.py
@@ -1,42 +1,26 @@
-import copy
-import os
-import re
-import string
 import sys
-from collections import Counter, defaultdict, deque, namedtuple
-from contextlib import suppress
-from dataclasses import dataclass
-from functools import cache, cmp_to_key, reduce
-from io import StringIO
-from itertools import permutations  # noqa
-from itertools import batched, chain, combinations, count, cycle, product, zip_longest
-from math import ceil, floor, lcm, prod, sqrt
+from itertools import batched, combinations, product
 from pathlib import Path
-from pprint import PrettyPrinter
 
 
 def solve(graph, expansion):
+    xs, ys = set(range(len(graph))), set(range(len(graph[0])))
     galaxies = [
-        (x, y)
+        (xs.discard(x) or x, ys.discard(y) or y)
         for x, y in product(range(len(graph)), range(len(graph[0])))
         if graph[x][y] == "#"
     ]
-    xs = [x for x, line in enumerate(graph) if "#" not in line]
-    graph = list(zip(*graph[::-1]))
-    ys = [x for x, line in enumerate(graph) if "#" not in line]
-    print(xs)
-    print(ys)
     galaxies = [
         (
-            (x + (next((i for i, v in enumerate(xs) if v > x), 0) * expansion)),
-            (y + (next((i for i, v in enumerate(ys) if v > y), 0) * expansion)),
+            (x + (sum(x > dx for dx in xs) * (expansion - 1))),
+            (y + (sum(y > dy for dy in ys) * (expansion - 1))),
         )
         for x, y in galaxies
     ]
-    return sum(abs(a - b) for a, b in combinations(galaxies, 2))
+    return sum(abs(c - d) for a, b in combinations(galaxies, 2) for c, d in zip(a, b))
 
 
 if __name__ == "__main__":
     lines = Path(sys.argv[1]).read_text().splitlines()
     print("Part 1:", solve(lines, 2))
-    # print("Part 2:", solve(lines, 1000000))
+    print("Part 2:", solve(lines, 1_000_000))
