99941b3da969ccfb965f4903f4f8ceb8a977d9c6
Alfred Gamulo <alfred.gamulo@gmail.com>
2023-12-21 14:11:43 -0500

    a little faster

--- 2023/21/main.py
+++ 2023/21/main.py
@@ -1,32 +1,9 @@
 import sys
 from collections import deque
-from itertools import product
 from pathlib import Path
 
 
-def expand(plots, boundaries):
-    new = set()
-    neighbors = list(product([0, -1, 1], repeat=2))
-    multiplier = boundaries[1] // len(lines)
-    boundaries = (
-        boundaries[0] - len(lines) * multiplier,
-        boundaries[1] + len(lines) * multiplier,
-    )
-    for r, c in plots:
-        new.update(
-            [
-                (
-                    r + n[0] * multiplier * len(lines),
-                    c + n[1] * multiplier * len(lines),
-                )
-                for n in neighbors
-            ]
-        )
-    plots.update(new)
-    return plots, boundaries
-
-
-def solve(limit, plots, boundaries):
+def solve(limit):
     neighbors = ((0, 1), (0, -1), (1, 0), (-1, 0))
     possibilities = deque(((0, start),))
     visited = set([start])
@@ -39,9 +16,7 @@ def solve(limit, plots, boundaries):
             answer += 1
         for n in neighbors:
             dr, dc = position[0] + n[0], position[1] + n[1]
-            if any(not boundaries[0] <= x <= boundaries[1] for x in [dr, dc]):
-                plots, boundaries = expand(plots, boundaries)
-            if (dr, dc) not in visited and (dr, dc) in plots:
+            if (dr, dc) not in visited and (dr % len(lines), dc % len(lines)) in plots:
                 possibilities.append((steps + 1, (dr, dc)))
                 visited.add((dr, dc))
     return answer
@@ -54,5 +29,7 @@ if __name__ == "__main__":
         for c, l in enumerate(line):
             if l != "#":
                 plots.add((r, c))
-    print("Part 1:", solve(6, plots, (0, len(lines))))
-    print("Part 2:", solve(1000, plots, (0, len(lines))))
+    print("Part 1:", solve(6))
+    print("Part 2:", solve(100))
+    print("Part 2:", solve(500))
+    print("Part 2:", solve(1000))
