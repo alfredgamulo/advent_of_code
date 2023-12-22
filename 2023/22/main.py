ea01ba1bff72ebf571613353b6546e45be08197b
Alfred Gamulo <alfred.gamulo@gmail.com>
2023-12-22 15:02:04 -0500

    checkpoint

--- 2023/22/main.py
+++ 2023/22/main.py
@@ -1,16 +1,10 @@
 import re
 import sys
-from collections import Counter, OrderedDict, defaultdict, deque, namedtuple
-from contextlib import suppress
-from copy import copy, deepcopy
-from dataclasses import dataclass
-from functools import cache, cmp_to_key, reduce
+from collections import defaultdict
+from copy import copy
 from heapq import heappop, heappush
-from io import StringIO
 from itertools import chain
-from math import ceil, floor, lcm, prod, sqrt
 from pathlib import Path
-from pprint import PrettyPrinter
 
 
 def intersect(a, b):
@@ -24,14 +18,16 @@ def drop(bricks):
         z, x, y = heappop(bricks)
         level = len(settled)
         while checks := settled[level]:
-            if any(
-                intersect(check[1], x) and intersect(check[2], y) for check in checks
-            ):
-                level += 1
-            else:
+            collision = False
+            for check in checks:
+                if intersect(check[1], x) and intersect(check[2], y):
+                    collision = True
+                    level = check[0][1] + 1
+            if not collision:
                 break
         z = (level, z[1] - z[0] + level)
         settled[level].append([z, x, y])
+    # print(settled)
     heap = []
     for brick in chain(*settled.values()):
         heappush(heap, brick)
@@ -41,17 +37,12 @@ def drop(bricks):
 def part1():
     settled = drop(copy(bricks))
     count = 0
-    print(settled)
     for i in range(len(settled)):
-        print()
         a = copy(settled)
         del a[i]
         b = copy(a)
-        print(a)
         if a == (c := drop(b)):
-            print(i)
             count += 1
-        print(c)
     return count
 
 
@@ -61,7 +52,6 @@ def part2():
 
 if __name__ == "__main__":
     bricks = []
-
     for line in Path(sys.argv[1]).read_text().splitlines():
         x1, y1, z1, x2, y2, z2 = map(int, re.findall("\\d+", line))
         heappush(bricks, [(z1, z2), (x1, x2), (y1, y2)])
