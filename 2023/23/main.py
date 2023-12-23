e01fdbdb561af82584284cb65ec75142bff3cff9
Alfred Gamulo <alfred.gamulo@gmail.com>
2023-12-23 14:41:10 -0500

    checkpoint

--- 2023/23/main.py
+++ 2023/23/main.py
@@ -1,5 +1,5 @@
 import sys
-
+from contextlib import suppress
 from heapq import heappop, heappush
 from itertools import product
 from pathlib import Path
@@ -9,14 +9,11 @@ def solve(slippery=True):
     hikes = [(0, start, [start])]
     neighbors = ((0, 1), (0, -1), (1, 0), (-1, 0))
     violation = {(0, 1): "<", (0, -1): ">", (1, 0): "^", (-1, 0): "v"}
-    duplicate = dict()
     completes = []
     while hikes:
         steps, cursor, history = heappop(hikes)
         if cursor == end:
             heappush(completes, (steps, cursor, history))
-        if cursor in duplicate and duplicate[cursor] > steps:
-            continue
         for n in neighbors:
             dr, dc = cursor[0] + n[0], cursor[1] + n[1]
             if slippery and (dr, dc) in slopes and slopes[(dr, dc)] == violation[n]:
@@ -29,8 +26,16 @@ def solve(slippery=True):
 if __name__ == "__main__":
     lines = Path(sys.argv[1]).read_text().splitlines()
     start, end = (0, 1), (len(lines) - 1, len(lines[0]) - 2)
-    paths, slopes = set(), dict()
+    paths, slopes, junctions = set(), dict(), set()
+    neighbors = ((0, 1), (0, -1), (1, 0), (-1, 0))
     for r, c in product(range(len(lines)), repeat=2):
+        surrounding = []
+        for n in neighbors:
+            dr, dc = r + n[0], c + n[1]
+            with suppress(IndexError):
+                surrounding.append(lines[dr][dc] in "^>v<")
+        if 3 <= sum(surrounding):
+            junctions.add((r, c))
         if lines[r][c] in ".^>v<":
             paths.add((r, c))
         if lines[r][c] in "^>v<":
