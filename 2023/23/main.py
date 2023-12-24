ca2fd1c7d25a27d75d3b689f13f7350a71fff29b
Alfred Gamulo <alfred.gamulo@gmail.com>
2023-12-23 20:17:08 -0500

    checkpoint

--- 2023/23/main.py
+++ 2023/23/main.py
@@ -1,4 +1,5 @@
 import sys
+from collections import deque
 from contextlib import suppress
 from heapq import heappop, heappush
 from itertools import product
@@ -7,27 +8,43 @@ from pathlib import Path
 
 def solve(slippery=True):
     hikes = [(0, start, set([start]))]
-    neighbors = ((0, 1), (0, -1), (1, 0), (-1, 0))
     violation = {(0, 1): "<", (0, -1): ">", (1, 0): "^", (-1, 0): "v"}
     most = 0
     while hikes:
         steps, cursor, history = heappop(hikes)
+        while cursor not in junctions:
+            for n in neighbors:
+                dr, dc = cursor[0] + n[0], cursor[1] + n[1]
+                if (dr, dc) in paths and (dr, dc) not in history:
+                    steps -= 1
+                    history.add((dr, dc))
+                    cursor = (dr, dc)
+                    break
+            break
         if cursor == end:
-            if abs(steps) > most:
-                most = abs(steps)
+            if abs(steps) + magic_number > most:
+                most = abs(steps) + magic_number
                 print(most, flush=True)
+            continue
         for n in neighbors:
             dr, dc = cursor[0] + n[0], cursor[1] + n[1]
             if slippery and (dr, dc) in slopes and slopes[(dr, dc)] == violation[n]:
                 continue
             if (dr, dc) in paths and (dr, dc) not in history:
                 heappush(hikes, (steps - 1, (dr, dc), set(list(history) + [(dr, dc)])))
-    return most
+
+    return most  # magic number
 
 
 if __name__ == "__main__":
     lines = Path(sys.argv[1]).read_text().splitlines()
-    start, end = (0, 1), (len(lines) - 1, len(lines[0]) - 2)
+    start = (0, 1)
+    if sys.argv[1] == "sample":
+        end = (19, 19)
+        magic_number = 5
+    else:
+        end = (127, 123)
+        magic_number = 209
     paths, slopes, junctions = set(), dict(), set()
     neighbors = ((0, 1), (0, -1), (1, 0), (-1, 0))
     for r, c in product(range(len(lines)), repeat=2):
