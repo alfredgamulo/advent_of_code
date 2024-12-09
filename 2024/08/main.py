9cb2f1525857ff3c21aa58be1c11bfce55d5b9ee
Alfred Gamulo <alfred.gamulo@gmail.com>
2024-12-08 22:45:45 -0500

    cuter

--- 2024/08/main.py
+++ 2024/08/main.py
@@ -4,42 +4,26 @@ from itertools import combinations
 from pathlib import Path
 
 
-def part1(frequencies, limit):
+def solver(antennas, limit, part2=False):
     new_points = set()
     for antennas in frequencies.values():
-        if len(antennas) == 1:
-            continue
         for a, b in combinations(antennas, 2):
+            if part2:
+                new_points.add(a)
+                new_points.add(b)
             dx, dy = a[0] - b[0], a[1] - b[1]
-            x1, y1 = a[0] + dx, a[1] + dy
-            x2, y2 = b[0] - dx, b[1] - dy
-            if 0 <= x1 < limit and 0 <= y1 < limit:
-                new_points.add((x1, y1))
-            if 0 <= x2 < limit and 0 <= y2 < limit:
-                new_points.add((x2, y2))
-
-    return len(new_points)
-
-
-def part2(frequencies, limit):
-    new_points = set()
-    for antennas in frequencies.values():
-        if len(antennas) == 1:
-            continue
-        for a, b in combinations(antennas, 2):
-            new_points.add(a)
-            new_points.add(b)
-            dx, dy = a[0] - b[0], a[1] - b[1]
-            x1, y1 = a[0] + dx, a[1] + dy
-            while 0 <= x1 < limit and 0 <= y1 < limit:
-                new_points.add((x1, y1))
-                x1, y1 = x1 + dx, y1 + dy
-
-            x2, y2 = b[0] - dx, b[1] - dy
-            while 0 <= x2 < limit and 0 <= y2 < limit:
-                new_points.add((x2, y2))
-                x2, y2 = x2 - dx, y2 - dy
-
+            x, y = a[0] + dx, a[1] + dy
+            while 0 <= x < limit and 0 <= y < limit:
+                new_points.add((x, y))
+                if not part2:
+                    break
+                x, y = x + dx, y + dy
+            x, y = b[0] - dx, b[1] - dy
+            while 0 <= x < limit and 0 <= y < limit:
+                new_points.add((x, y))
+                if not part2:
+                    break
+                x, y = x - dx, y - dy
     return len(new_points)
 
 
@@ -51,5 +35,5 @@ if __name__ == "__main__":
             if c != ".":
                 frequencies[c].add((x, y))
     limit = len(lines)
-    print("Part 1:", part1(frequencies, limit))
-    print("Part 2:", part2(frequencies, limit))
+    print("Part 1:", solver(frequencies, limit))
+    print("Part 2:", solver(frequencies, limit, part2=True))
