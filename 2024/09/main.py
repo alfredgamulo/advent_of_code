134e50deed1b730f86cde32571788ce8a8d83cd5
Alfred Gamulo <alfred.gamulo@gmail.com>
2024-12-09 15:58:29 -0500

    golfing

--- 2024/09/main.py
+++ 2024/09/main.py
@@ -6,11 +6,10 @@ if __name__ == "__main__":
     lines = Path(sys.argv[1]).read_text().splitlines()
     disk = deque()
     for i, c in enumerate(lines[0]):
-        for _ in range(int(c)):
-            push = "."
-            if i % 2 == 0:
-                push = int(i) // 2
-            disk.append(push)
+        push = "."
+        if i % 2 == 0:
+            push = int(i) // 2
+        disk += [push] * int(c)
     for i in range(len(disk)):
         try:
             d = disk[i]
