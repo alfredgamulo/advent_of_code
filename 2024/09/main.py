7efad4e4e38c068bf1fc7a0992f2883debf26f2f
Alfred Gamulo <alfred.gamulo@gmail.com>
2024-12-09 16:13:07 -0500

    checkpoint

--- 2024/09/main.py
+++ 2024/09/main.py
@@ -6,21 +6,18 @@ if __name__ == "__main__":
     lines = Path(sys.argv[1]).read_text().splitlines()
     disk = deque()
     for i, c in enumerate(lines[0]):
-        push = "."
+        push = None
         if i % 2 == 0:
             push = int(i) // 2
         disk += [push] * int(c)
-    for i in range(len(disk)):
-        try:
-            d = disk[i]
-            if d != ".":
-                continue
-            popped = disk.pop()
-            while popped == ".":
-                popped = disk.pop()
-            disk[i] = popped
-        except IndexError:
-            break
+    new_disk = []
+    while disk:
+        push = disk.popleft()
+        while push == None and disk:
+            push = disk.pop()
+        if push is not None:
+            new_disk.append(push)
+    disk = new_disk
     print("".join(map(str, disk)))
     p1 = 0
     for i, d in enumerate(disk):
@@ -30,3 +27,5 @@ if __name__ == "__main__":
     print("Part 2:", None)
 
 # 6154086908998 too low
+# 6154096669449
+#
