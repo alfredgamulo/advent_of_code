5e94045144a9dcfddfcf8eeab7af9628fdd87f0d
Alfred Gamulo <alfred.gamulo@gmail.com>
2024-12-10 04:10:04 +0000

    golf

--- 2024/09/main.py
+++ 2024/09/main.py
@@ -19,10 +19,7 @@ def part1(lines):
         if push is not None:
             new_disk.append(push)
     disk = new_disk
-    p1 = 0
-    for i, d in enumerate(disk):
-        p1 += i * d
-    return p1
+    return sum(i * d for i, d in enumerate(disk))
 
 
 def part2(lines):
@@ -32,7 +29,6 @@ def part2(lines):
         if i % 2 == 0:
             push = int(i) // 2
         disk.append([push] * int(c))
-
     for i, d in reversed(list(enumerate(disk))):
         if None in d:
             continue
@@ -45,15 +41,10 @@ def part2(lines):
                 break
 
     chained = list(chain.from_iterable(disk))
-    p2 = 0
-    for i, d in enumerate(chained):
-        if d:
-            p2 += i * d
-    return p2
+    return sum(i * d if d is not None else 0 for i, d in enumerate(chained))
 
 
 if __name__ == "__main__":
     lines = Path(sys.argv[1]).read_text().splitlines()
-
     print("Part 1:", part1(lines))
     print("Part 2:", part2(lines))
