3add07491757a301d955d38af763aca07f387678
Alfred Gamulo <alfred.gamulo@gmail.com>
2024-12-12 22:53:03 +0000

    10 done

--- 2024/10/main.py
+++ 2024/10/main.py
@@ -1,55 +1,43 @@
-import copy
-import os
-import re
-import string
 import sys
-from collections import Counter, OrderedDict, defaultdict, deque, namedtuple
-from contextlib import suppress
-from dataclasses import dataclass
-from functools import cache, cmp_to_key, reduce
-from heapq import heappop, heappush
-from io import StringIO
-from itertools import (batched, chain, combinations, count, groupby,
-                       permutations, product, zip_longest)
-from math import ceil, floor, lcm, prod, sqrt
-from operator import index
+from collections import defaultdict, deque
 from pathlib import Path
-from pprint import PrettyPrinter
-
-import numpy as np
 
 
 def neighbors(coords):
     return [
-        (coords[0] - 1, coords[1] - 1),
-        (coords[0] - 1, coords[1] + 1),
-        (coords[0] + 1, coords[1] - 1),
-        (coords[0] + 1, coords[1] + 1),
+        (coords[0] - 1, coords[1]),
+        (coords[0] + 1, coords[1]),
+        (coords[0], coords[1] - 1),
+        (coords[0], coords[1] + 1),
     ]
 
 
-def part1(indexes, topography):
-    print(indexes)
-    print(topography)
-    zeroes = indexes[0]
+def solve(indexes):
     p1 = 0
-    for zero in zeroes:
-        print(zero)
-    ...
-
-
-def part2(indexes):
+    p2 = 0
+    for zero in indexes[0]:
+        search = deque([(0, zero)])
+        peaks = set()
+        while search:
+            height, location = search.popleft()
+            if height == 9:
+                peaks.add(location)
+                p2 += 1
+                continue
+            for n in neighbors(location):
+                if n in indexes[height + 1]:
+                    search.append((height + 1, n))
+        p1 += len(peaks)
+    return p1, p2
     ...
 
 
 if __name__ == "__main__":
     lines = Path(sys.argv[1]).read_text().splitlines()
-    print(lines)
     indexes = defaultdict(set)
-    topography = dict()
     for y, line in enumerate(lines):
         for x, c in enumerate(line):
             indexes[int(c)].add((x, y))
-            topography[(x, y)] = c
-    print("Part 1:", part1(indexes, topography))
-    print("Part 2:", part2(indexes))
+    p1, p2 = solve(indexes)
+    print("Part 1:", p1)
+    print("Part 2:", p2)
