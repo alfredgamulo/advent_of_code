70774577fd1411ed7c65a1bb30cd74c842066ebb
Alfred Gamulo <alfred.gamulo@gmail.com>
2025-11-16 01:29:17 +0000

    feat: update dev container

---
+++ 2018/17/main.py
@@ -0,0 +1,33 @@
+import copy
+import os
+import re
+import string
+import sys
+from collections import Counter, OrderedDict, defaultdict, deque, namedtuple
+from contextlib import suppress
+from dataclasses import dataclass
+from functools import cache, cmp_to_key, reduce
+from heapq import heappop, heappush
+from io import StringIO
+from itertools import (batched, chain, combinations, count, groupby,
+                       permutations, product, zip_longest)
+from math import ceil, floor, lcm, prod, sqrt
+from pathlib import Path
+from pprint import PrettyPrinter
+
+import numpy as np
+
+
+def part1(lines):
+    ...
+
+
+def part2(lines):
+    ...
+
+
+if __name__ == "__main__":
+    lines = Path(sys.argv[1]).read_text().splitlines()
+    print(lines)
+    print("Part 1:", part1(lines))
+    print("Part 2:", part2(lines))
